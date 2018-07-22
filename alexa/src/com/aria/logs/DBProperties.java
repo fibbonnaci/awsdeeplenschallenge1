a8b5d12f34431cca1f855565e45a16ddb3fe0642
macc0der <shanthan.kesharaju@gmail.com>
2018-07-22 11:51:33 -0400

    clean up

--- alexa/src/com/aria/logs/DBProperties.java
+++
@@ -1,7 +0,0 @@
-package com.mytutor.logs;
-
-public class DBProperties {
-	public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	public static final String JDBC_URL="jdbc:mysql://204.236.197.53:3306/123Math?user=shan&password=2lfemS5w0DIBlidi";
-
-}
